5d16098a270217ceb006869c0deab8fd63b40aab
Bashar-Syouri <beshoo_1993@hotmail.com>
2021-05-02 18:02:01 +0300

    edit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let hours=['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+let Seattle = {
+    Location: 'Seattle',
+    Min: 23,
+    MAx:65,
+    AvgCookie:6.3,
+    hourlySales:[],
+    dialySales:0,
+}
